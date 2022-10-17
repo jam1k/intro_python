be51c9d9827e78d7dc2fae0ff99642c9756a63c6
Jamilya Shestovskaya <jamilya@Jamilyas-MBP.vitaly.home>
2022-10-17 15:33:40 +0300

    removing empty

--- part11-04_lengths/src/lengths.py
+++
@@ -1,3 +0,0 @@
-# WRITE YOUR SOLUTION HERE:
-def lengths(lists: list):
-    return [len(lst) for lst in lists]
--- part11-11_lengths_of_strings/src/lengths_of_strings.py
+++
@@ -1,3 +0,0 @@
-# WRITE YOUR SOLUTION HERE:
-def lengths(strings: list):
-    return {string : len(string) for string in strings}
